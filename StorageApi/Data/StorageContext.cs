baefea25aa2256aca8697f7befa3db9c6ca268d6
malindesire <malindesirejohansson@gmail.com>
2025-06-26 14:38:27 +0200

    Create Controller

---
+++ StorageApi/Data/StorageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StorageApi.Models;
+
+namespace StorageApi.Data
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext (DbContextOptions<StorageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StorageApi.Models.Product> Product { get; set; } = default!;
+    }
+}
