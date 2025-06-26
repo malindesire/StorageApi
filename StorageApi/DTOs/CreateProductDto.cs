66cc2bea1b233cf63aadfde407cd68847a731ba2
malindesire <malindesirejohansson@gmail.com>
2025-06-26 16:30:40 +0200

    Add DTOs folder with ProductDto classes

---
+++ StorageApi/DTOs/CreateProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.DTOs
+{
+    public class CreateProductDto
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be a positive number.")]
+        public int Price { get; set; }
+
+        [StringLength(50, ErrorMessage = "Category cannot be longer than 50 characters.")]
+        public string Category { get; set; } = string.Empty;
+        public string Shelf { get; set; } = string.Empty;
+        public int Count { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ StorageApi/DTOs/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace StorageApi.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; }
+        public string Name { get; } = string.Empty;
+        public int Price { get; }
+        public int Count { get; }
+        public int InventoryValue => Price * Count;
+
+    }
+}
