e9d31b63db102b2eb98fd9ba91d5646df4dac8f7
malindesire <malindesirejohansson@gmail.com>
2025-07-02 14:08:54 +0200

    Add ProductStatsDto

---
+++ StorageApi/DTOs/ProductStatsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace StorageApi.DTOs
+{
+    public class ProductStatsDto
+    {
+        public int TotalProductsCount { get; set; }
+        public int TotalInventoryValue { get; set; }
+        public int AveragePrice { get; set; }
+    }
+}
